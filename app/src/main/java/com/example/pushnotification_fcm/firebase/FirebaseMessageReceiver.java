018a94cd892c4c743526365ef1071604f559430c
Aman <eyeon4840@gmail.com>
2022-11-01 15:10:52 +0530

    Push notification comment updated

--- app/src/main/java/com/example/pushnotification_fcm/firebase/FirebaseMessageReceiver.java
+++ app/src/main/java/com/example/pushnotification_fcm/firebase/FirebaseMessageReceiver.java
@@ -15,13 +15,24 @@ import com.example.pushnotification_fcm.MainActivity;
 import com.example.pushnotification_fcm.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
+/*
+ * FirebaseMessagingService - This service is required to do any type of message handling beyond just receiving notifications,
+ *                                           while the client app runs in the background.
+ */
 public class FirebaseMessageReceiver extends FirebaseMessagingService {
 
+    /*
+    *  onMessageReceived --   handle 2 events:
+    * 1. If Notification contains any data payload from server
+    * 2. ...............contains any notification payload  (sent via the Firebase Admin SDK)
+    */
     @Override
-    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {       // RemoteMessage: is the object of the message passed using FCM
 
         if (remoteMessage.getNotification() != null) {
+            /*
+            *     Here, 'title' and 'message' are the assumed names of JSON attributes ( here we don't have any data payload-- in this case we exract title & message from LIST)
+            * */
             String title = remoteMessage.getNotification().getTitle();
             String body = remoteMessage.getNotification().getBody();
 
@@ -35,19 +46,24 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
 
         Intent intent = new Intent(this, MainActivity.class);
         String channel_id = "notification_channel";
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);        // to clear the activity stack till now.
 
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), channel_id).setSmallIcon(R.drawable.ic_smile).setAutoCancel(true).setVibrate(new long[]{1000, 1000, 1000, 1000, 1000}).setOnlyAlertOnce(true).setContentIntent(pendingIntent);
 
+        /*
+        * A customized design for the notification can be set only for Android versions 4.1 and above
+        * */
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             builder = builder.setContent(getCustomDesign(title, message));
-
         } else {
+            // customized layout cannot be used , thus layout is set here
             builder = builder.setContentTitle(title).setContentText(message).setSmallIcon(R.drawable.ic_smile);
         }
-
+/*
+* Obj, to Notify the user of event  that happen in the background.
+* */
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         // Check if the Android Version is greater than Oreo
--- readMe.md
+++ readMe.md
@@ -1,2 +1,5 @@
 # Intro : 
 Uses FireBase Cloud Messagining to send PushNotification.
+
+## Src: 
+[Geek4Geek](https://www.geeksforgeeks.org/how-to-push-notification-in-android-using-firebase-cloud-messaging/)
